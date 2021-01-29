3fce8e0d13d12a3d220a1dfda3138ee1d30ccd7a
sarah0311 <73232369+sarah0311@users.noreply.github.com>
2021-01-29 12:03:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,61 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint = Matter.Constraint;
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	roof = new Roof(width/2, 100, 400, 30);
+
+	bobObject1 = new Bob(235, 450, 80);
+	bobObject2 = new Bob(315, 450, 80);
+	bobObject3 = new Bob(395, 450, 80);
+	bobObject4 = new Bob(475, 450, 80);
+	bobObject5 = new Bob(555, 450, 80);
+
+	rope1 = new Rope(bobObject1.body,roof.body, -160, 0);
+	rope2 = new Rope(bobObject2.body,roof.body, -80, 0);
+	rope3 = new Rope(bobObject3.body,roof.body, 0, 0);
+	rope4 = new Rope(bobObject4.body,roof.body, 80, 0);
+	rope5 = new Rope(bobObject5.body,roof.body, 160, 0);
+
+	Engine.run(engine);
+
+	keyPressed();
+}
+
+
+function draw() {
+  background("lightgrey");
+  rectMode(CENTER);
+  Engine.update(engine);
+  
+  roof.display();
+  
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+
+  bobObject1.display();
+  bobObject2.display();
+  bobObject3.display();
+  bobObject4.display();
+  bobObject5.display();
+
+  drawSprites();
+ 
+}
+function keyPressed() { 
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(bobObject1.body,bobObject1.body.position,{x:-500,y:-500}); 
+	} 
+} 
